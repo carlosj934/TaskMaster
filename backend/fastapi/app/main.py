68ce8acba5f54d41137b703d53bdfe6f6927f3a3
test1 <carlosjcorona934@gmail.com>
2025-07-24 17:36:43 -0700

    routes for user login and registration. still a work in progress

--- backend/fastapi/app/main.py
+++ backend/fastapi/app/main.py
@@ -1,4 +1,28 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from typing import Annotated
+
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from pydantic import BaseModel, EmailStr
+import requests
+
+
+class userRegister(BaseModel):
+    fullName: str
+    userName: str
+    email: EmailStr
+    password: str
+
+class userLogin(BaseModel):
+    userName: str
+    password: str
 
 app = FastAPI()
+
+@app.post("/auth/register", status_code=status.HTTP_201_CREATED)
+async def userRegister(userregister: userRegister):
+    pass
+
+@app.post("/auth/login", status_code=status.HTTP_201_CREATED)
+async def userLogin(userlogin: userLogin):
+    pass
+
